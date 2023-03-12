c2a66fc4582c59ef97b9cf8bd991803751a66921
manu0542 <58816119+manu0542@users.noreply.github.com>
2023-03-12 17:30:41 +0530

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,318 @@
+// margin();
+// function margin(){
+//    document.getElementById("cardView1").innerHTML="500px";
+// }
+function test(){
+   console.log("Touched")
+}
+function cCardListShowMiMobile(){
+   console.log(document.getElementById("CV").offsetHeight)
+   document.getElementById("secondNav").style.backgroundColor="#fff"
+   document.getElementById("cCardList").style.visibility="visible";
+   document.getElementById("cCardList").style.opacity="1";
+   document.getElementById("cCardList").style.zIndex="1";
+   document.getElementById("cCardList").style.transform="translateY(0)";
+   document.getElementById("cCardList").style.transitionDelay="0s,0s,0.3s";
+   document.getElementById("item1src").src="../images/MI Phones/1.png";
+   document.getElementById("item2src").src="../images/MI Phones/mi2.png"
+   document.getElementById("item3src").src="../images/MI Phones/mi3.png"
+   document.getElementById("item4src").src="../images/MI Phones/mi4.jpg"
+   document.getElementById("item5src").src="../images/MI Phones/mi5.png"
+   document.getElementById("item6src").src="../images/MI Phones/mi6.png"
+   document.getElementById("item1Cost").innerHTML="₹From 69,999";
+   document.getElementById("item2Cost").innerHTML="₹From 39,999";
+   document.getElementById("item3Cost").innerHTML="₹From 29,999";
+   document.getElementById("item4Cost").innerHTML="₹From 21,999";
+   document.getElementById("item5Cost").innerHTML="₹From 32,999";
+   document.getElementById("item6Cost").innerHTML="₹From 20,999";
+   document.getElementById("item1Name").innerHTML="Mi 11 Ultra 5G";
+   document.getElementById("item2Name").innerHTML="Mi 11X Pro 5G";
+   document.getElementById("item3Name").innerHTML="Mi 11X 5G";
+   document.getElementById("item4Name").innerHTML="Mi 11 Lite";
+   document.getElementById("item5Name").innerHTML="Mi 10T";
+   document.getElementById("item6Name").innerHTML="Mi 10i";
+}
+function cCardListShowRmMobile(){
+    document.getElementById("secondNav").style.backgroundColor="#fff"
+    document.getElementById("cCardList").style.visibility="visible";
+    document.getElementById("cCardList").style.opacity="1";
+    document.getElementById("cCardList").style.zIndex="1";
+    document.getElementById("cCardList").style.transform="translateY(0)";
+    document.getElementById("cCardList").style.transitionDelay="0s,0s,0.3s";
+    document.getElementById("item1src").src="../images/RM phones/rm1.png";
+    document.getElementById("item2src").src="../images/RM phones/rm2.png"
+    document.getElementById("item3src").src="../images/RM phones/rm3.jpg"
+    document.getElementById("item4src").src="../images/RM phones/rm4.png"
+    document.getElementById("item5src").src="../images/RM phones/rm5.png"
+    document.getElementById("item6src").src="../images/RM phones/rm6.png"
+    document.getElementById("item1Cost").innerHTML="From ₹ 18,999";
+    document.getElementById("item2Cost").innerHTML="From ₹ 15,999"
+    document.getElementById("item3Cost").innerHTML="From ₹ 14,999"
+    document.getElementById("item4Cost").innerHTML="From ₹ 12,999"
+    document.getElementById("item5Cost").innerHTML="From ₹ 10,999"
+    document.getElementById("item6Cost").innerHTML=""
+    document.getElementById("item1Name").innerHTML="Redmi Note 10 Pro Max";
+    document.getElementById("item2Name").innerHTML="Redmi Note 10 Pro";
+    document.getElementById("item3Name").innerHTML="Redmi Note 10S";
+    document.getElementById("item4Name").innerHTML="Redmi Note 10";
+    document.getElementById("item5Name").innerHTML="Redmi 9 Power";
+    document.getElementById("item6Name").innerHTML="View More";
+ }
+ function cCardListShowTv(){
+    document.getElementById("secondNav").style.backgroundColor="#fff"
+    document.getElementById("cCardList").style.visibility="visible";
+    document.getElementById("cCardList").style.opacity="1";
+    document.getElementById("cCardList").style.zIndex="1";
+    document.getElementById("cCardList").style.transform="translateY(0)";
+    document.getElementById("cCardList").style.transitionDelay="0s,0s,0.3s";
+    document.getElementById("item1src").src="../images/tv/tv1.jpg";
+    document.getElementById("item2src").src="../images/tv/tv2.jpg"
+    document.getElementById("item3src").src="../images/tv/tv3.png"
+    document.getElementById("item4src").src="../images/tv/tv4.png"
+    document.getElementById("item5src").src="../images/tv/tv5.jpg"
+    document.getElementById("item6src").src="../images/RM phones/rm6.png"
+    document.getElementById("item1Cost").innerHTML="₹ 1,27,999";
+    document.getElementById("item2Cost").innerHTML="₹ 59,999"
+    document.getElementById("item3Cost").innerHTML="₹ 37,999"
+    document.getElementById("item4Cost").innerHTML="₹ 27,999"
+    document.getElementById("item5Cost").innerHTML="₹ 24,999"
+    document.getElementById("item6Cost").innerHTML=""
+    document.getElementById("item1Name").innerHTML="Mi QLED TV 189.34cm (75)";
+    document.getElementById("item2Name").innerHTML="Mi QLED TV 4K 138.8 cm (55)";
+    document.getElementById("item3Name").innerHTML="Redmi Smart TV X Series";
+    document.getElementById("item4Name").innerHTML="Mi TV 4A 108 cm (43)";
+    document.getElementById("item5Name").innerHTML="Mi TV 4A 100 cm (40)";
+    document.getElementById("item6Name").innerHTML="View More";
+ }
+ function cCardListShowLap(){
+    document.getElementById("secondNav").style.backgroundColor="#fff"
+    document.getElementById("cCardList").style.visibility="visible";
+    document.getElementById("cCardList").style.opacity="1";
+    document.getElementById("cCardList").style.zIndex="1";
+    document.getElementById("cCardList").style.transform="translateY(0)";
+    document.getElementById("cCardList").style.transitionDelay="0s,0s,0.3s";
+    document.getElementById("item1src").src="../images/lap/l1.png";
+    document.getElementById("item2src").src="../images/lap/l2.jpg"
+    document.getElementById("item3src").src="../images/lap/l3.png"
+    document.getElementById("item4src").src="../images/lap/l4.png"
+    document.getElementById("item5src").src="../images/lap/l2.jpg"
+    document.getElementById("item6src").src="../images/RM phones/rm6.png"
+    document.getElementById("item1Cost").innerHTML="From ₹ 54,999";
+    document.getElementById("item2Cost").innerHTML="From ₹ 44,999"
+    document.getElementById("item3Cost").innerHTML="From ₹ 41,999"
+    document.getElementById("item4Cost").innerHTML="₹ 38,999"
+    document.getElementById("item5Cost").innerHTML="₹ 24,999"
+    document.getElementById("item6Cost").innerHTML=""
+    document.getElementById("item1Name").innerHTML="Mi Notebook 14 Horizon";
+    document.getElementById("item2Name").innerHTML="Mi Notebook 14(IC)";
+    document.getElementById("item3Name").innerHTML="Mi Notebook 14";
+    document.getElementById("item4Name").innerHTML="Mi NoteBook 14e-Learning Edition";
+    document.getElementById("item5Name").innerHTML="Mi Notebook 14(IC)";
+    document.getElementById("item6Name").innerHTML="View More";
+ }
+ function cCardListShowFit(){
+    document.getElementById("secondNav").style.backgroundColor="#fff"
+    document.getElementById("cCardList").style.visibility="visible";
+    document.getElementById("cCardList").style.opacity="1";
+    document.getElementById("cCardList").style.zIndex="1";
+    document.getElementById("cCardList").style.transform="translateY(0)";
+    document.getElementById("cCardList").style.transitionDelay="0s,0s,0.3s";
+    document.getElementById("item1src").src="../images/fl/fl1.jpg";
+    document.getElementById("item2src").src="../images/fl/fl2.jpg"
+    document.getElementById("item3src").src="../images/fl/fl3.png"
+    document.getElementById("item4src").src="../images/fl/fl4.png"
+    document.getElementById("item5src").src="../images/fl/fl5.png"
+    document.getElementById("item6src").src="../images/RM phones/rm6.png"
+    document.getElementById("item1Cost").innerHTML="From ₹ 54,999";
+    document.getElementById("item2Cost").innerHTML="From ₹ 44,999"
+    document.getElementById("item3Cost").innerHTML="From ₹ 41,999"
+    document.getElementById("item4Cost").innerHTML="₹ 38,999"
+    document.getElementById("item5Cost").innerHTML="₹ 24,999"
+    document.getElementById("item6Cost").innerHTML=""
+    document.getElementById("item1Name").innerHTML="Mi Watch Revolve Active";
+    document.getElementById("item2Name").innerHTML="Redmi Watch GPS";
+    document.getElementById("item3Name").innerHTML="Mi Smart Band 5";
+    document.getElementById("item4Name").innerHTML="Mi Beard Trimmer 1C";
+    document.getElementById("item5Name").innerHTML="Mi Athleisure Shoes";
+    document.getElementById("item6Name").innerHTML="View More";
+ }
+ function cCardListShowHome(){
+    document.getElementById("secondNav").style.backgroundColor="#fff"
+    document.getElementById("cCardList").style.visibility="visible";
+    document.getElementById("cCardList").style.opacity="1";
+    document.getElementById("cCardList").style.zIndex="1";
+    document.getElementById("cCardList").style.transform="translateY(0)";
+    document.getElementById("cCardList").style.transitionDelay="0s,0s,0.3s";
+    document.getElementById("item1src").src="../images/home/h1.png";
+    document.getElementById("item2src").src="../images/home/h2.jpg"
+    document.getElementById("item3src").src="../images/home/h3.jpg"
+    document.getElementById("item4src").src="../images/home/h4.jpg"
+    document.getElementById("item5src").src="../images/home/h5.png"
+    document.getElementById("item6src").src="../images/RM phones/rm6.png"
+    document.getElementById("item1Cost").innerHTML="₹ 24,999";
+    document.getElementById("item2Cost").innerHTML="₹ 10,999"
+    document.getElementById("item3Cost").innerHTML="₹ 6,999"
+    document.getElementById("item4Cost").innerHTML="₹ 12,999"
+    document.getElementById("item5Cost").innerHTML="From ₹ 1,799"
+    document.getElementById("item6Cost").innerHTML=""
+    document.getElementById("item1Name").innerHTML="Mi Robot Vacuum-Mop P";
+    document.getElementById("item2Name").innerHTML="Mi Air Purifier 3";
+    document.getElementById("item3Name").innerHTML="Mi Air Purifier 2C";
+    document.getElementById("item4Name").innerHTML="Mi Smart Water Purifier";
+    document.getElementById("item5Name").innerHTML="Mi Security Cameras";
+    document.getElementById("item6Name").innerHTML="View More";
+ }
+ 
+ function cCardListShowAudio(){
+    document.getElementById("secondNav").style.backgroundColor="#fff"
+    document.getElementById("cCardList").style.visibility="visible";
+    document.getElementById("cCardList").style.opacity="1";
+    document.getElementById("cCardList").style.zIndex="1";
+    document.getElementById("cCardList").style.transform="translateY(0)";
+    document.getElementById("cCardList").style.transitionDelay="0s,0s,0.3s";
+    document.getElementById("item1src").src="../images/audio/a1.png";
+    document.getElementById("item2src").src="../images/audio/a2.png"
+    document.getElementById("item3src").src="../images/audio/a3.png"
+    document.getElementById("item4src").src="../images/audio/a4.png"
+    document.getElementById("item5src").src="../images/audio/a5.png"
+    document.getElementById("item6src").src="../images/RM phones/rm6.png"
+    document.getElementById("item1Cost").innerHTML="₹ 3,999";
+    document.getElementById("item2Cost").innerHTML="₹ 3,999"
+    document.getElementById("item3Cost").innerHTML="₹ 1,499"
+    document.getElementById("item4Cost").innerHTML="₹ 1,499"
+    document.getElementById("item5Cost").innerHTML="₹ 399"
+    document.getElementById("item6Cost").innerHTML=""
+    document.getElementById("item1Name").innerHTML="Mi Smart Speaker";
+    document.getElementById("item2Name").innerHTML="Mi True Wireless Earphones 2";
+    document.getElementById("item3Name").innerHTML="Redmi Earbuds 2C";
+    document.getElementById("item4Name").innerHTML="Mi Outdoor Bluetooth Speaker (5W)";
+    document.getElementById("item5Name").innerHTML="Redmi Earphones";
+    document.getElementById("item6Name").innerHTML="View More";
+ }
+ function cCardListShowAccess(){
+    document.getElementById("secondNav").style.backgroundColor="#fff"
+    document.getElementById("cCardList").style.visibility="visible";
+    document.getElementById("cCardList").style.opacity="1";
+    document.getElementById("cCardList").style.zIndex="1";
+    document.getElementById("cCardList").style.transform="translateY(0)";
+    document.getElementById("cCardList").style.transitionDelay="0s,0s,0.3s";
+    document.getElementById("item1src").src="../images/acces/a1.jpg";
+    document.getElementById("item2src").src="../images/acces/a2.jpg"
+    document.getElementById("item3src").src="../images/acces/a3.jpg"
+    document.getElementById("item4src").src="../images/acces/a4.png"
+    document.getElementById("item5src").src="../images/acces/a5.png"
+    document.getElementById("item6src").src="../images/RM phones/rm6.png"
+    document.getElementById("item1Cost").innerHTML="From ₹ 799";
+    document.getElementById("item2Cost").innerHTML="₹ 999"
+    document.getElementById("item3Cost").innerHTML="₹ 500"
+    document.getElementById("item4Cost").innerHTML="From ₹ 179"
+    document.getElementById("item5Cost").innerHTML="From ₹ 49"
+    document.getElementById("item6Cost").innerHTML=""
+    document.getElementById("item1Name").innerHTML="Power Banks";
+    document.getElementById("item2Name").innerHTML="Mi Router 4C";
+    document.getElementById("item3Name").innerHTML="Mi Portable Wireless Mouse";
+    document.getElementById("item4Name").innerHTML="Chargers & Cables";
+    document.getElementById("item5Name").innerHTML="Cases & Protectors";
+    document.getElementById("item6Name").innerHTML="View More";
+ }
+function cCardListHide(){
+   document.getElementById("secondNav").style.backgroundColor="transparent"
+    document.getElementById("cCardList").style.visibility="hidden";
+    document.getElementById("cCardList").style.opacity="0";
+    document.getElementById("cCardList").style.position="fixed";
+    document.getElementById("cCardList").style.paddingTop="0.3vw";
+    document.getElementById("cCardList").style.transform="translateY(-3em)";
+    document.getElementById("cCardList").style.zIndex="0";
+    document.getElementById("cCardList").style.transition="all 0.3s ease 0s,visibility 0s linear 0.3s , z-index 0s linear 0.1s";
+}
+function setMS()
+{
+   document.getElementById("img1").src="../images/MS/ms1.jpg"
+   document.getElementById("img2").src="../images/MS/ms2.jpg"
+   document.getElementById("img3").src="../images/MS/ms3.png"
+   document.getElementById("img4").src="../images/MS/ms4.jpg"
+   document.getElementById("img5").src="../images/MS/ms5.jpg"
+   document.getElementById("img6").src="../images/MS/ms6.jpg"
+   document.getElementById("img7").src="../images/MS/ms7.png"
+}
+function setSD()
+{
+   document.getElementById("cose2").innerHTML="₹ 2,099";
+   document.getElementById("cose3").innerHTML="₹ 12,999"
+   document.getElementById("cose4").innerHTML="₹ 9,999"
+   document.getElementById("cose5").innerHTML="₹ 2,499"
+   document.getElementById("cose6").innerHTML="₹ 2,499"
+   document.getElementById("name2").innerHTML="Mi Smart Band 4";
+   document.getElementById("name3").innerHTML="Mi Smart Water Purifier";
+   document.getElementById("name4").innerHTML="Mi Watch Revolve";
+   document.getElementById("name5").innerHTML="Mi Smart Bedside Lamp 2";
+   document.getElementById("name6").innerHTML="Mi Smart LED Desk Lamp 1S";
+   document.getElementById("img1").src="../images/SD/sd1.jpg"
+   document.getElementById("img2").src="../images/SD/sd2.jpg"
+   document.getElementById("img3").src="../images/SD/sd3.jpg"
+   document.getElementById("img4").src="../images/SD/sd4.png"
+   document.getElementById("img5").src="../images/SD/sd5.jpg"
+   document.getElementById("img6").src="../images/SD/sd6.jpg"
+   document.getElementById("img7").src="../images/MS/ms7.png"
+}
+function setH()
+{
+
+   document.getElementById("cose2").innerHTML="₹ 10,999";
+   document.getElementById("cose3").innerHTML="₹ 6,999"
+   document.getElementById("cose4").innerHTML="₹ 999"
+   document.getElementById("cose5").innerHTML="₹ 3,499"
+   document.getElementById("cose6").innerHTML="₹ 1,299"
+   document.getElementById("name2").innerHTML="Mi Air Purifier 3";
+   document.getElementById("name3").innerHTML="Mi Air Purifier 2C";
+   document.getElementById("name4").innerHTML="Mi Router 4C";
+   document.getElementById("name5").innerHTML="Mi Box 4K";
+   document.getElementById("name6").innerHTML="Mi LED Wi-Fi Smart Bulb";
+   document.getElementById("img1").src="../images/HO/h1.jpg"
+   document.getElementById("img2").src="../images/HO/h2.jpg"
+   document.getElementById("img3").src="../images/HO/h3.jpg"
+   document.getElementById("img4").src="../images/HO/h4.jpg"
+   document.getElementById("img5").src="../images/HO/h5.png"
+   document.getElementById("img6").src="../images/HO/h6.jpg"
+   document.getElementById("img7").src="../images/MS/ms7.png"
+}
+function setLS()
+{
+document.getElementById("cose2").innerHTML="₹ 1,999";
+document.getElementById("cose3").innerHTML="₹ 1,699"
+document.getElementById("cose4").innerHTML="₹ 1,499"
+document.getElementById("cose5").innerHTML="₹ 399"
+document.getElementById("cose6").innerHTML="₹ 2,499"
+document.getElementById("name2").innerHTML="Mi Travel Backpack";
+document.getElementById("name3").innerHTML="Mi Athleisure Shoes";
+document.getElementById("name4").innerHTML="Mi Beard Trimmer";
+document.getElementById("name5").innerHTML="Mi KN-95 Protective Mask (Pack of 5)";
+document.getElementById("name6").innerHTML="Mi Portable Electric Air Compressor";
+   document.getElementById("img1").src="../images/ls/l1.jpg"
+   document.getElementById("img2").src="../images/ls/l2.png"
+   document.getElementById("img3").src="../images/ls/l3.png"
+   document.getElementById("img4").src="../images/ls/l4.jpg"
+   document.getElementById("img5").src="../images/ls/l5.png"
+   document.getElementById("img6").src="../images/ls/l6.png"
+   document.getElementById("img7").src="../images/MS/ms7.png"
+}
+function setMA()
+{
+document.getElementById("cose2").innerHTML="₹ 49 onwards";
+document.getElementById("cose3").innerHTML="₹ 179 onwards"
+document.getElementById("cose4").innerHTML="₹ 2,499"
+document.getElementById("cose5").innerHTML="₹ 999"
+document.getElementById("cose6").innerHTML="₹ 999"
+document.getElementById("name2").innerHTML="Cases & Covers";
+document.getElementById("name3").innerHTML="Charger & USB Gadgets";
+document.getElementById("name4").innerHTML="Mi Wireless Power Bank 10000mAh";
+document.getElementById("name5").innerHTML="10000mAh Mi Power Bank 3i";
+document.getElementById("name6").innerHTML="Redmi SonicBass Wireless Earphones";
+   document.getElementById("img1").src="../images/ma/m1.png"
+   document.getElementById("img2").src="../images/ma/m2.jpg"
+   document.getElementById("img3").src="../images/ma/m3.jpg"
+   document.getElementById("img4").src="../images/ma/m4.jpg"
+   document.getElementById("img5").src="../images/ma/m5.png"
+   document.getElementById("img6").src="../images/ma/m6.png"
+   document.getElementById("img7").src="../images/MS/ms7.png"
+}
